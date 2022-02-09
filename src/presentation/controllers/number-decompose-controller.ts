a61ff25cdcb22b079d60b630c7d7911ccf20953a
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-09 01:01:40 -0300

    feat: should return 400 if number is no a numeric type

--- src/presentation/controllers/number-decompose-controller.ts
+++ src/presentation/controllers/number-decompose-controller.ts
@@ -1,7 +1,21 @@
 class NumberDecomposeController {
     handle(httpRequest: any): any {
+        if (!httpRequest.body.number) {
+            return {
+                statusCode: 400,
+                body: new Error('O número obrigatório!'),
+            };
+        }
+
+        if (typeof httpRequest.body.number !== 'number') {
+            return {
+                statusCode: 400,
+                body: new Error('Formato do número inválido!'),
+            };
+        }
+
         return {
-            statusCode: 400,
+            statusCode: 200,
         };
     }
 }
