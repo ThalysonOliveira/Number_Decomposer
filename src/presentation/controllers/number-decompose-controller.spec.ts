d9c760ef7ed93fd0e21d45f86ce2cecf823cdb8f
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-09 01:28:34 -0300

    test: should return 400 if number provided is less to be one

--- src/presentation/controllers/number-decompose-controller.spec.ts
+++ src/presentation/controllers/number-decompose-controller.spec.ts
@@ -11,6 +11,7 @@ describe('Number Decompose Controller', () => {
         const httpResponse = sut.handle(httpRequest);
 
         expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error('O número obrigatório!'));
     });
 
     test('Should return 400 if number is no a numeric type', () => {
@@ -25,5 +26,21 @@ describe('Number Decompose Controller', () => {
         const httpResponse = sut.handle(httpRequest);
 
         expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error('Formato do número inválido!'));
+    });
+
+    test('Should return 400 if number provided is less to be one', () => {
+        const sut = new NumberDecomposeController();
+
+        const httpRequest = {
+            body: {
+                number: -1,
+            },
+        };
+
+        const httpResponse = sut.handle(httpRequest);
+
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error('O número fornecido não pode ser menor que um!'));
     });
 });
