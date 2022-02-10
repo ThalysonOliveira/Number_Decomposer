a7ab54461df184228920f16bfffe7c8c8cbe0ea3
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-10 11:46:08 -0300

    feat: ensure return the number decomposed on success

--- src/data/services/number-decomposer-service.ts
+++ src/data/services/number-decomposer-service.ts
@@ -2,11 +2,25 @@ import { Decomposed, NumberDecomposer } from '../../domain/userCases/number-deco
 
 class NumberDecomposerService implements NumberDecomposer {
     decomposer(number: number): Decomposed {
-        const decomposed = {
+        const decomposed: Decomposed = {
             number,
             dividingNumbers: [],
-            primeNumbers: [],
+            primeNumbers: [1],
         };
+
+        for (let i = 1; i <= number; i++) {
+            if (number % i === 0) decomposed.dividingNumbers.push(i);
+        }
+
+        for (const numbers of decomposed.dividingNumbers) {
+            let numberOfDivisor = 0;
+
+            for (let i = 1; i <= numbers; i++) {
+                if (numbers % i === 0) numberOfDivisor++;
+            }
+
+            if (numberOfDivisor === 2) decomposed.primeNumbers.push(numbers);
+        }
 
         return decomposed;
     }
