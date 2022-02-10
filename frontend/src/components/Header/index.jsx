2b18b9a6cde2376aab9260ff695874c22c77599b
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-10 15:44:34 -0300

    feat: create header

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <h1>Decompositor de números</h1>
+    </header>
+  );
+}
