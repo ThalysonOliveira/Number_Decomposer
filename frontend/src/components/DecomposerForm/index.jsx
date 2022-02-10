c50647befcbbdd6d6add70178ef3d419c8a2d478
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-10 19:17:44 -0300

    refactor: api implementation

--- frontend/src/components/DecomposerForm/index.jsx
+++ frontend/src/components/DecomposerForm/index.jsx
@@ -2,30 +2,56 @@ import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// import { api } from '../services/api';
+import { api } from '../../services/api';
 import { Result } from '../Result';
 
 import styles from './styles.module.scss';
 
 export function DecomposerForm() {
   const [number, setNumber] = useState('');
+  const [inputNumber, setInputNumber] = useState('');
+  const [dividingNumbers, setDividingNumbers] = useState([]);
+  const [primeNumbers, setPrimeNumbers] = useState([]);
 
   async function handleDecomposer(e) {
     e.preventDefault();
 
-    if (number === '') {
+    setInputNumber(Number(inputNumber));
+
+    if (inputNumber === '') {
       toast.error('Digite um número');
+      setInputNumber('');
+      setNumber('');
+      setDividingNumbers([]);
+      setPrimeNumbers([]);
       return;
     }
 
-    // const response = await api.post('/transactions', {
-    //   number,
-    // });
-    // const { transaction } = response.data;
+    if (inputNumber <= 0) {
+      toast.error('O número fornecido não pode ser menor que um!');
+      setInputNumber('');
+      setNumber('');
+      setDividingNumbers([]);
+      setPrimeNumbers([]);
+      return;
+    }
 
-    alert(`Decompondo número ${number}`);
+    const response = await api.post('/number-decomposer', {
+      number: Number(inputNumber),
+    });
+    const { data } = response.data;
+    console.log(data);
+    console.log(response);
 
-    setNumber('');
+    if (!response.ok) {
+      toast.error(response.message);
+    }
+
+    toast.success(response.data.message);
+    setNumber(data?.number);
+    setDividingNumbers(data?.dividingNumbers);
+    setPrimeNumbers(data?.primeNumbers);
+    setInputNumber('');
   }
 
   return (
@@ -36,13 +62,17 @@ export function DecomposerForm() {
         <input
           type="number"
           placeholder="Digite o número a decompor"
-          value={number}
-          onChange={(event) => setNumber(event.target.value)}
+          value={inputNumber}
+          onChange={(event) => setInputNumber(event.target.value)}
         />
         <button type="submit">Decompor</button>
       </form>
 
-      <Result />
+      <Result
+        number={number}
+        dividingNumbers={dividingNumbers}
+        primeNumbers={primeNumbers}
+      />
     </>
   );
 }
--- frontend/src/components/Result/index.jsx
+++ frontend/src/components/Result/index.jsx
@@ -1,16 +1,16 @@
 import styles from './styles.module.scss';
 
-export function Result() {
+export function Result({ number, dividingNumbers, primeNumbers }) {
   return (
     <section className={styles.resultContainer}>
       <p>
-        Número decomposto: <span>41</span>
+        Número decomposto: <span>{number}</span>
       </p>
       <p>
-        Números divisores: <span>1, 3, 5</span>
+        Números divisores: <span>{dividingNumbers.join(', ')}</span>
       </p>
       <p>
-        Divisores primos: <span>1, 3, 5</span>
+        Divisores primos: <span>{primeNumbers.join(', ')}</span>
       </p>
     </section>
   );
