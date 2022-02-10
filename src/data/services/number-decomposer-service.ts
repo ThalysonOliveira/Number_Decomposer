9e73a570f6f05a6b015263fc00fe5f8aac2118c0
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-10 00:15:12 -0300

    feat: ensure call NumberDecomposerService.decompose with correct values

---
+++ src/data/services/number-decomposer-service.ts
@@ -0,0 +1,10 @@
+import { Decomposed, NumberDecomposer } from '../../domain/userCases/number-decomposer';
+
+class NumberDecomposerService implements NumberDecomposer {
+    // eslint-disable-next-line no-unused-vars
+    decomposer(number: number): Decomposed {
+        return null as any;
+    }
+}
+
+export { NumberDecomposerService };
