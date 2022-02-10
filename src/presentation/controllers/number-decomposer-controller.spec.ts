69e477e21eb95e0510b15aea1a93ef09ff39f09d
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-09 23:08:39 -0300

    test: ensure call NumberDecomposer with correct value

--- src/presentation/controllers/number-decomposer-controller.spec.ts
+++ src/presentation/controllers/number-decomposer-controller.spec.ts
@@ -1,15 +1,39 @@
+import { Decomposed, NumberDecomposer } from '../../domain/userCases/number-decomposer';
 import { badRequest } from '../helpers/http-helpers';
 import { NumberDecomposerController } from './number-decomposer-controller';
 
-const makeSut = (): NumberDecomposerController => {
-    const sut = new NumberDecomposerController();
+type SutTypes = {
+    sut: NumberDecomposerController
+    numberDecomposerStub: NumberDecomposer
+}
 
-    return sut;
+const makeNumberDecomposer = (): NumberDecomposer => {
+    class NumberDecomposerStub implements NumberDecomposer {
+        // eslint-disable-next-line no-unused-vars
+        decomposer(number: number): Decomposed {
+            const fakeNumberDecomposeD = {
+                number: 45,
+                dividingNumbers: [1, 3, 5, 9, 15, 45],
+                primeNumbers: [1, 3, 5],
+            };
+
+            return fakeNumberDecomposeD;
+        }
+    }
+
+    return new NumberDecomposerStub();
+};
+
+const makeSut = (): SutTypes => {
+    const numberDecomposerStub = makeNumberDecomposer();
+    const sut = new NumberDecomposerController(numberDecomposerStub);
+
+    return { sut, numberDecomposerStub };
 };
 
 describe('Number Decomposer Controller', () => {
     test('Should return 400 if no number is provided', () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
 
         const httpRequest = {
             body: {},
@@ -17,11 +41,11 @@ describe('Number Decomposer Controller', () => {
 
         const httpResponse = sut.handle(httpRequest);
 
-        expect(httpResponse).toEqual(badRequest('O número obrigatório!'));
+        expect(httpResponse).toEqual(badRequest('O número é obrigatório!'));
     });
 
     test('Should return 400 if number is no a numeric type', () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
 
         const httpRequest = {
             body: {
@@ -35,7 +59,7 @@ describe('Number Decomposer Controller', () => {
     });
 
     test('Should return 400 if number provided is less to be one', () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
 
         const httpRequest = {
             body: {
@@ -46,5 +70,21 @@ describe('Number Decomposer Controller', () => {
         const httpResponse = sut.handle(httpRequest);
 
         expect(httpResponse).toEqual(badRequest('O número fornecido não pode ser menor que um!'));
+    });
+
+    test('Should call NumberDecomposer with correct value', () => {
+        const { sut, numberDecomposerStub } = makeSut();
+
+        const spyNumberDecomposerStub = jest.spyOn(numberDecomposerStub, 'decomposer');
+
+        const httpRequest = {
+            body: {
+                number: 1,
+            },
+        };
+
+        sut.handle(httpRequest);
+
+        expect(spyNumberDecomposerStub).toHaveBeenCalledWith(httpRequest.body.number);
     });
 });
