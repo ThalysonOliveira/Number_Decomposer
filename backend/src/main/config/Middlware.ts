44ad7ff036f23e91c9465d35b7d959bdf8380606
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-11 12:20:55 -0300

    chore: add and implemts swagger

--- backend/src/main/config/Middlware.ts
+++ backend/src/main/config/Middlware.ts
@@ -2,6 +2,8 @@ import { Express, json, urlencoded } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import Log from 'node-color-log';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger.json';
 
 class Middlewares {
     constructor(app: Express) {
@@ -9,6 +11,7 @@ class Middlewares {
         app.use(helmet());
         app.use(urlencoded({ extended: true }));
         app.use(json());
+        app.use('/number-decomposer/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
         Log.info('Importação de middlewares concluídos');
     }
