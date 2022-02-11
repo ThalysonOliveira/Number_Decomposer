0e256d6a651253e366947e638ad4fcd6f8ac764f
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-02-11 14:43:54 -0300

    refactor: rename api variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const { API_BASE_URL } = process.env
-
 export const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: process.env.REACT_APP_BASE_API,
 });
